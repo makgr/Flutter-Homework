730e54f845778d7bcc3643f7a8438ed67b6646e0
Mohammad Ali Khan <xvirus.bd@gmail.com>
2021-10-31 06:08:13 +0600

    Initial commit

---
+++ flutter_homework2/android/app/src/main/kotlin/com/example/flutter_homework2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_homework2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_homework2/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: UserSignUp(),
+  ));
+}
+
+class UserSignUp extends StatelessWidget{
+
+  @override 
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign up'),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+                hintText: 'Enter UserName eg. baboy, abc123 etc',
+                
+              ),
+              keyboardType: TextInputType.text ,
+            ),
+
+            SizedBox(height: 12,),
+
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+                hintText: 'Enter Email eg. abc@mail.com',
+              ),
+              keyboardType: TextInputType.emailAddress,
+              
+            ),
+
+            SizedBox(height: 12,),
+
+            TextField(
+              obscureText: true,  
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                hintText: 'Enter Password eg. 123@abc',
+
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
